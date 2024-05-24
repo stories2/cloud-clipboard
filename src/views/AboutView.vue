86776a66cb2097e2ef50600c146882a461b92615
Kim HyunWoo <stories2@naver.com>
2024-05-24 23:18:25 +0900

    asdfads

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="about" style="margin-top: 20vh">
     <h1>Copy and paste your content freely</h1>
-    <pre>
-Send content you want to paste to another device.
-Read the QR code using the device you want to paste it.
-That's it!
-    </pre>
+    <div>
+      Send content you want to paste to another device.<br />
+      Read the QR code using the device you want to paste it.<br />
+      That's it!
+    </div>
   </div>
 </template>
 
