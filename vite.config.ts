c1c23b5e68d36c5b683b0560f047bd57abab03e6
Kim HyunWoo <stories2@naver.com>
2025-04-01 23:28:53 +0900

    asdf

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { fileURLToPath, URL } from 'node:url'
+// import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import nightwatchPlugin from 'vite-plugin-nightwatch'
+import path from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,7 +13,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': path.resolve(__dirname, "./src"),
     }
   }
 })
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,7 @@
-import { fileURLToPath } from 'node:url'
+// import { fileURLToPath } from 'node:url'
 import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
 import viteConfig from './vite.config'
+import path from 'node:path'
 
 export default mergeConfig(
   viteConfig,
@@ -8,7 +9,7 @@ export default mergeConfig(
     test: {
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/**'],
-      root: fileURLToPath(new URL('./', import.meta.url))
+      root: path.resolve(__dirname, "./"),
     }
   })
 )
